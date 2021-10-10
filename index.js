761f49ff37af46a8d0bbc7eec8a2810bb95bf09f
flow <flow@enceladus.orbit>
2021-10-10 19:34:21 +0200

    fix syntax

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const PATH = require( 'path' );
 module.exports = {
-  expansions: PATH.join __dirname, 'britfone.expansions.3.0.1.tsv',
-  main:       PATH.join __dirname, 'britfone.main.3.0.1.csv',
-  symbols:    PATH.join __dirname, 'britfone.symbols.3.0.1.txt',
+  expansions: PATH.join( __dirname, 'britfone.expansions.3.0.1.tsv' ),
+  main:       PATH.join( __dirname, 'britfone.main.3.0.1.csv'       ),
+  symbols:    PATH.join( __dirname, 'britfone.symbols.3.0.1.txt'    ),
   };
